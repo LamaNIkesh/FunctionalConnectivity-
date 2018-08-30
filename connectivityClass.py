954b4825e84d692baafe93c4ce9c80a3b52ddc4b
nikesh <nikesh.strat@yahoo.com>
2018-08-30 09:58:35 +0100

    notebook added

--- connectivityClass.py
+++ connectivityClass.py
@@ -28,78 +28,73 @@ class MatrixGenerate(object):
 	def __init__(self):
 		pass
 
-	def generatePandasDF(self,adjacencyMatrix = ''):
-		AdjacencyArray = np.load(adjacencyMatrix)
-		print(AdjacencyArray.shape[0])
+	def generatePandasDF(self,synchronyFileLocation):
+		#Loading array with synchrony levels
+		AdjacencyArray = np.load(synchronyFileLocation)
+		#AdjacencyArray = np.load('isi_distance.npy')
 		#rows/columns size -- since we are dealing with a square matrix one info is enough
-		rows_columns = AdjacencyArray.shape[0]
+		rows_count = AdjacencyArray.shape[0] 
+		columns_count = AdjacencyArray.shape[1]
 
-		df = pd.DataFrame(AdjacencyArray, index = AdjacencyArray[:,0])
-		print(df)
-		links = df.reset_index()
-		links.columns = ['var1','var2','value']
-		links
-		return df,links
+		#lets rearrange the square matrix into a 3 column matirx where first and second columns
+		#are channels and third is the synchorny between them. we will put these into a panda dataframe
+
+		#df = pd.DataFrame(columns = ['Ch_A', 'Ch_B', 'SyncValue'])
+
+		#empty list which will be populated
+		# basically the data is in a nXn matrix. What I want to get is a lists of list of three elements --> channelA, channelB and correlation between them
+		# This results in a 3 column matrix. This will later be converted into panda dataframe to fit into networkx library 
+		dataframeList = []
+		#print(dataframeList)
+
+		# looping through the numpy array, since the channel number is not explicity given but rather only the pairwise correlation is provided
+		# with each iteration, the indexes are taken as channel numbers. 
+
+		##TODO --> Spatial information
+
+		for i in range(200): #only using 200 rows for testing
+			for j in range(200): #only using 200 columns for testing
+				#print('Ch_A: {} -- Ch_B: {} -- SyncValue: {}'.format(i,j,AdjacencyArray[i,j]))
+				#df.append({'Ch_A':i, 'Ch_B':j, 'SyncValue': AdjacencyArray[i,j]}, ignore_index = True)
+				if AdjacencyArray[i][j] < 0.4 :
+					AdjacencyArray[i][j] = 0
+				elif AdjacencyArray[i][j] == 1:
+					AdjacencyArray[i][j] = 0.8
+				else:
+					pass
+				if AdjacencyArray[i][j]>0:	
+					data_input = [i,j,AdjacencyArray[i][j]]
+					dataframeList.append(data_input)
+				#dataframeList[i].append(j)
+				#dataframeList[i].append(AdjacencyArray[i,j])
+				pass
+
+			#print('Row {} done processing!!'.format(i))        
+		print (dataframeList)
+
+		df = pd.DataFrame(dataframeList, columns = ['Ch_A', 'Ch_B', 'SyncValue'])
+		#print(df)
+		links = df.loc[(df['SyncValue'] > 0.8) & (df['Ch_A'] != df['Ch_B'])]
+		print (links)
+		return links
 
 
 	def GenerateGraphFromPandas(self, links):
 		G  = nx.from_pandas_edgelist(links, 'Ch_A','Ch_B')
 		#plot the network
-		nx.draw(G, with_labels=False, node_color = 'red', node_size = 50, edge_color = 'black', linewidths = 1, font_size = 4,grid = True)
+		nx.draw(G, with_labels=True, node_color = 'red', node_size = 50, edge_color = 'black', linewidths = 1, font_size = 4,grid = True)
 		plt.show()
 
+'''
+if __name__ == "__main__":
 
-#Loading array with synchrony levels
-AdjacencyArray = np.load('isi_distance.npy')
-#print(AdjacencyArray.shape[0])
-#rows/columns size -- since we are dealing with a square matrix one info is enough
-rows_count = AdjacencyArray.shape[0] #
-columns_count = AdjacencyArray.shape[1]
-
-#lets rearrange the square matrix into a 3 column matirx where first and second columns
-#are channels and third is the synchorny between them. we will put these into a panda dataframe
-
-#df = pd.DataFrame(columns = ['Ch_A', 'Ch_B', 'SyncValue'])
-
-#empty list which will be populated
-# basically the data is in a nXn matrix. What I want to get is a lists of list of three elements --> channelA, channelB and correlation between them
-# This results in a 3 column matrix. This will later be converted into panda dataframe to fit into networkx library 
-dataframeList = []
-print(dataframeList)
-
-# looping through the numpy array, since the channel number is not explicity given but rather only the pairwise correlation is provided
-# with each iteration, the indexes are taken as channel numbers. 
-
-##TODO --> Spatial information
-
-for i in range(200): #only using 10 rows for testing
-	for j in range(200): #only using 10 columns for testing
-		#print('Ch_A: {} -- Ch_B: {} -- SyncValue: {}'.format(i,j,AdjacencyArray[i,j]))
-		#df.append({'Ch_A':i, 'Ch_B':j, 'SyncValue': AdjacencyArray[i,j]}, ignore_index = True)
-		if AdjacencyArray[i][j] < 0.4 :
-			AdjacencyArray[i][j] = 0
-		elif AdjacencyArray[i][j] == 1:
-			AdjacencyArray[i][j] = 0.8
-		else:
-			pass
-		if AdjacencyArray[i][j]>0:	
-			data_input = [i,j,AdjacencyArray[i][j]]
-			dataframeList.append(data_input)
-		#dataframeList[i].append(j)
-		#dataframeList[i].append(AdjacencyArray[i,j])
-		pass
-
-	print('Row {} done processing!!'.format(i))        
-print (dataframeList)
-
-df = pd.DataFrame(dataframeList, columns = ['Ch_A', 'Ch_B', 'SyncValue'])
-print(df)
-links = df.loc[(df['SyncValue'] > 0.8) & (df['Ch_A'] != df['Ch_B'])]
-print (links)
-
-M = MatrixGenerate()
-M.GenerateGraphFromPandas(links)
-
+	#lets createa an istance of MatrixGenerate class
+	M = MatrixGenerate()
+	
+	SynchronyFileLocation = 'isi_distance.npy'
+	links = M.generatePandasDF(SynchronyFileLocation)
+	M.GenerateGraphFromPandas(links)
+'''
 
 
 
